ef47a659e58e7eedcf6448885e3fed4fe420a948
huangje1 <jennyhuang@BridgeUp-003.T-mobile.com>
2018-01-18 20:58:43 -0500

    MAD commits.
    #perna reference

--- MyApplication3/app/src/main/java/com/example/jennyhuang/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/jennyhuang/myapplication/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.jennyhuang.myapplication;
 
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    Animation anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        anim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fadein);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.startAnimation(anim);
+
+            }
+        });
     }
 }
